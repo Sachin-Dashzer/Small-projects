bb9b9ae33ee39bef51f82bafded4b17185d0a297
sachin_dashzer <sachin8287037611@gmail.com>
2023-09-05 21:12:32 +0530

    last commit

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -4,11 +4,10 @@ import React, { useRef, useEffect, Lazy, Suspense, lazy } from "react";
 const emailjs = lazy(() => import("emailjs-com"));
 
 import "./footer.css";
-import Ending from "./Ending/Ending.jsx";
+
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-
 
 const Footer = () => {
   const form = useRef();
@@ -85,9 +84,11 @@ const Footer = () => {
               <div>
                 <input type="submit" value="submit" className="btn" />
                 <h5>
-                <i className="fa-brands fa-whatsapp"></i>                  <span> +91 8287037611 </span>
+                  <i className="fa-brands fa-whatsapp"></i>{" "}
+                  <span> +91 8287037611 </span>
                   <br />
-                  <i className="fa-solid fa-envelope-circle-check"></i>                  <span>sachin8287037611@gmail.com</span>
+                  <i className="fa-solid fa-envelope-circle-check"></i>{" "}
+                  <span>sachin8287037611@gmail.com</span>
                 </h5>
               </div>
             </form>
@@ -103,7 +104,15 @@ const Footer = () => {
         </section>
       </Suspense>
 
-      <Ending />
+      <section id="Ending">
+        <h3>
+          "Hard work is like a bad joke you might not always get the punchline
+          right away, <br /> but when you finally do, it's a dream come true!"
+        </h3>
+        <h4>
+          All right reserved : <span>Sachin_Dashzer</span>
+        </h4>
+      </section>
     </>
   );
 };
