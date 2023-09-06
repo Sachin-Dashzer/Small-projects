da3a2699d80f26a2f0bf454562f46204b5a7421b
sachin_dashzer <sachin8287037611@gmail.com>
2023-09-06 20:39:13 +0530

    ss

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+
+module: {
+  rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader', // You need Babel for JSX
+      },
+    },
+  ],
+},
+
 })
